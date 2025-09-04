e5759252030c86ee98d6dd93382b32e1442b1738
Jack Waterloo <jack.waterloo77@gmail.com>
2025-09-04 00:06:08 -0500

    APIs both fetch and display

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import Giphy from './js/giphy';
 import Weather from './js/weather';
 
 // since fetchGif is async, it returns a promise
-Giphy.fetchGif('cat').then((jsonData) => {
-  const img = document.querySelector('img');
-  img.src = jsonData.data[0].images.original.url;
-  img.alt = jsonData.data[0].alt_text;
-});
 
-Weather.getWeatherData('atlanta').then((data) => {
-  console.log('Received data:\n', data);
+Weather.getWeatherData('tokyo').then((weatherData) => {
+  console.log('Received weather function data:\n', weatherData);
+
+  Giphy.fetchGif(weatherData.icon + " weather").then((gifData) => {
+    console.log(gifData);
+    const img = document.createElement('img');
+    const div = document.createElement('div');
+    for (const key in weatherData) {
+      const p = document.createElement('p');
+      p.textContent = weatherData[key];
+      div.appendChild(p);
+    }
+    img.setAttribute("src",gifData.src);
+    img.setAttribute("alt",gifData.altText);
+    div.appendChild(img);
+    const body = document.querySelector("body");
+    body.appendChild(div);
+  });
 });

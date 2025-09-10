1be0f94eecb645f2be9ed7d2c8abf51557edd432
Jack Waterloo <jack.waterloo77@gmail.com>
2025-09-09 23:40:37 -0500

    local storage works

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import './styles/main.css';
 import Giphy from './js/giphy';
 import Weather from './js/weather';
 import DomHandler from './js/domHandler';
+import StorageHandler from './js/storageHandler';
 
 // since fetchGif is async, it returns a promise
 
 const form = document.querySelector('form');
-const domeWeatherObjects = [];
+let domeWeatherObjects = [];
+
+// loads data if present in local storage
+const localStorageData = StorageHandler.loadData();
+if (localStorageData.length > 0) {
+  domeWeatherObjects = domeWeatherObjects.concat(localStorageData);
+  DomHandler.displayObjects(domeWeatherObjects);
+}
 
 // turn this into async
 form.addEventListener('submit', (e) => {
@@ -19,13 +27,12 @@ form.addEventListener('submit', (e) => {
       console.log(gifData);
       const domWeatherObject = { ...weatherData, ...gifData };
       domeWeatherObjects.push(domWeatherObject);
+      StorageHandler.saveData(domWeatherObject);
 
       DomHandler.displayObjects(domeWeatherObjects);
 
     });
   });
 });
-
-// Create DOM manipulation class so that data can be auto updated into DOM
 
 // Save to local storage
---
+++ src/js/storageHandler.js
@@ -0,0 +1,27 @@
+export default class StorageHandler {
+  /**
+   * Loads all data from localStorage and returns it as an array.
+   * @returns {Array} Array of parsed objects from localStorage.
+   */
+  static loadData(){
+    if (localStorage.length === 0) {
+      return [];
+    }
+
+    let data = [];
+
+    Object.keys(localStorage).forEach((key)=> {
+      data = data.concat(JSON.parse(localStorage.getItem(key)));
+    });
+
+    return data;
+  }
+
+  static saveData(objectArray){
+    localStorage.setItem(crypto.randomUUID(),JSON.stringify(objectArray));
+  }
+
+  static clearData(){
+    localStorage.clear();
+  }
+}
